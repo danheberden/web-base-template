f7809dff67eb2013aee4b259699450fbfe960f05
Dan Heberden <danheberden@gmail.com>
2012-10-15 13:24:42 -0400

    Add grunt and basic task

---
+++ grunt.js
@@ -0,0 +1,40 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.loadTasks( 'grunt-tasks' );
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    test: {
+      files: ['test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint test'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true
+      },
+      globals: {
+        jQuery: true
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+
+};
